dcca57cf7eaca7bbdfc61ceb42be311624ea6cc5
carlobortolan <carlobortolan@gmail.com>
2023-08-15 20:39:30 +0200

    Initial Commit

---
+++ mobile/mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobileApp.swift
+//  mobile
+//
+//  Created by cb on 15.08.23.
+//
+
+import SwiftUI
+
+@main
+struct mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
