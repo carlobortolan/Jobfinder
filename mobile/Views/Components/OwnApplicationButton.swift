96d7d46d3ba708d13ca263967ee4a2b47c540f55
Carlo Bortolan <carlobortolan@gmail.com>
2023-09-09 03:35:30 +0200

    Updated assets

---
+++ mobile/Views/Components/OwnApplicationButton.swift
@@ -0,0 +1,20 @@
+//
+//  OwnApplicationButton.swift
+//  mobile
+//
+//  Created by cb on 09.09.23.
+//
+
+import SwiftUI
+
+struct OwnApplicationButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OwnApplicationButton_Previews: PreviewProvider {
+    static var previews: some View {
+        OwnApplicationButton()
+    }
+}
